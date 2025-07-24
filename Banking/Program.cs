1bd04775993985d8186877bf89e7e745d66e8264
Igor Simovic <igor.simovic@gmail.com>
2025-07-25 01:33:29 +0200

    Add project files.

---
+++ Banking/Program.cs
@@ -0,0 +1,14 @@
+﻿using Banking;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+// Register services
+builder.Services.AddTransient<TestRunService>();
+
+var app = builder.Build();
+
+// Resolve and run your service
+var testRunService = app.Services.GetRequiredService<TestRunService>();
+testRunService.Run();
---
+++ Banking/TestRunService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Banking
+{
+    public class TestRunService
+    {
+        private readonly ILogger<TestRunService> _logger;
+
+        public TestRunService(ILogger<TestRunService> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Run()
+        {
+            _logger.LogInformation("Start Test Run Service");
+
+
+            _logger.LogInformation("End Test Run Service");
+        }
+    }
+}
